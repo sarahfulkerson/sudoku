5bf5e2b0f697e8505938dd6dd20422673b26938f
Sarah Fulkerson <contact@sarahfulkerson.com>
2021-03-05 21:59:57 -0600

    commit 2!
    added text colors to tell puzzle and user entered values apart; added puzzle validation; added option to reset puzzle; added shelve DB to store puzzle state

--- lib.py
+++ lib.py
@@ -2,10 +2,22 @@ from collections.abc import MutableSequence
 
 import copy, sys
 
+class gcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 class Grid(MutableSequence):
     def __init__(self, arg):
         self.arg = arg
         self.puzzle = copy.deepcopy(self.arg)
+        self.values = [1,2,3,4,5,6,7,8,9,'erase']
     def __delitem__(self, index):
         return self.arg.__delitem__(index)
     def __getitem__(self, index):
@@ -24,40 +36,115 @@ class Grid(MutableSequence):
         print(cols)
         for x in self.puzzle:
             ind = self.puzzle.index(x)
-            row = 'R%s ||%s|%s|%s| |%s|%s|%s| |%s|%s|%s||' % (ind+1,x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7],x[8],)
             if ind % 3 == 0:
                 print(horizontal)
-            print(row)
-
+            print(f"R{ind+1}  |",end='')
+            for y in range(len(x)):
+                item = self.puzzle[ind][y]
+                formatted = ''
+                if item == self.arg[ind][y]:
+                    formatted = f"{gcolors.OKBLUE}{item}{gcolors.ENDC}"
+                    print(formatted,end='|')
+                else:
+                    formatted = f"{gcolors.WARNING}{item}{gcolors.ENDC}"
+                    print(formatted,end='|')
+                
+                if y % 3 == 2 and y != 8: print(' |',end='')
+            print('')
+        if ind % 3 == 0:
+            print(horizontal)
         print(horizontal)
     def fillbox(self,row,col,val):
+        # Validates that the user provided an allowable row
+        if int(row) not in self.values[:-1]:
+            print(int(row), self.values[:-1])
+            print(f"\n{gcolors.FAIL}Error: Not a valid row!{gcolors.ENDC}\n", file=sys.stderr)
+            self.display()
+            return
+
+        # Validates that the user provided an allowable col
+        if int(col) not in self.values[:-1]:
+            print(f"\n{gcolors.FAIL}Error: Not a valid column!{gcolors.ENDC}\n", file=sys.stderr)
+            self.display()
+            return
+
         rowindex = int(row)-1
-        colindex = int(col)-1
-        values = ['1','2','3','4','5','6','7','8','9','erase']
+        colindex = int(col)-1        
 
         # Validates that the user provided an allowable value
-        if str(val) not in values:
-            print("Error: Not an allowable value!\n", file=sys.stderr)
+        if int(val) not in self.values:
+            print(f"\n{gcolors.FAIL}Error: Not an allowable value!{gcolors.ENDC}\n", file=sys.stderr)
             self.display()
             return
 
         # Validates the user input against the original puzzle
         if self.arg[rowindex][colindex] != ' ':
-            print("Error: Can't change original puzzle!\n", file=sys.stderr)
+            print(f"\n{gcolors.FAIL}Error: Can't change original puzzle!{gcolors.ENDC}\n", file=sys.stderr)
             self.display()
             return
         
         # Either erase or place a number as requested by the user
         if str(val).lower() == 'erase':
             self.puzzle[rowindex][colindex] = ' '
-            print("Erased value at R%s, C%s\n" % (row,col))
+            print(f"\n{gcolors.OKCYAN}Erased value at R{row}, C{col}\n{gcolors.ENDC}")
         else:
-            self.puzzle[rowindex][colindex] = val
-            print("%s placed in R%s, C%s\n" % (val,row,col))
+            self.puzzle[rowindex][colindex] = int(val)
+            print(f"\n{gcolors.OKCYAN}{val} placed in R{row}, C{col}\n{gcolors.ENDC}")
         self.display()
     def isfull(self):
-        for x in self.puzzle:
-            for y in x:
-                if y == ' ':
-                    return False
+        # Determine if every box in the grid has a value
+        if set(map(lambda x: ' ' in x, self.puzzle)) != {False}: return False
         return True
+    def isCorrect(self):
+        # Validate each row in self.puzzle; return false if a row does not contain all values
+        if self.validate(self.puzzle) == False:
+            print('False: Row')
+            return False
+        
+        # Create list of column values in self.puzzle and validate each column
+        cols = self.getCols()
+        if self.validate(cols) == False: 
+            return False
+
+        # Create list of square values in self.puzzle and validate each square
+        squares = self.getSquares()
+        if self.validate(squares) == False: 
+            return False
+
+        # If all validation has passed then you have the solution!
+        return True
+    def validate(self, item):
+        seq = set(self.values[:-1])
+        if set(map(lambda row: set(row) == seq,item)) != {True}: 
+            print(list(map(lambda row: set(row) == seq,item)))
+            return False
+        return True
+    def getCols(self):
+        cols = [[],[],[],[],[],[],[],[],[]]
+        for x in range(len(self.puzzle)):
+            cols[0].extend([self.puzzle[0][x]])
+            cols[1].extend([self.puzzle[1][x]])
+            cols[2].extend([self.puzzle[2][x]])
+            cols[3].extend([self.puzzle[3][x]])
+            cols[4].extend([self.puzzle[4][x]])
+            cols[5].extend([self.puzzle[5][x]])
+            cols[6].extend([self.puzzle[6][x]])
+            cols[7].extend([self.puzzle[7][x]])
+            cols[8].extend([self.puzzle[8][x]])
+        return cols
+    def getSquares(self):
+        squares = [[],[],[],[],[],[],[],[],[]]
+        place = [0,1,2]
+        counter = 0
+        for row in self.puzzle:
+            squareA, squareB, squareC = row[:3], row[3:6], row[6:]
+            squares[place[0]].extend(squareA)
+            squares[place[1]].extend(squareB)
+            squares[place[2]].extend(squareC)
+
+            if counter % 3 == 2:
+                place = [x + 3 for x in place]
+            counter += 1
+        return squares
+    def reset(self):
+        self.puzzle = copy.deepcopy(self.arg)
--- main.py
+++ main.py
@@ -2,10 +2,27 @@
 """
 A fully playable game of sudoku written in python!
 """
-from lib import Grid
+from lib import Grid, gcolors
 
+import shelve, sys
+
+helpString = """
+How to play:
+
+- Type 'help' for game instructions.
+- Type 'reset' to erase all squares on the board.
+- Type 'save' to save your current progress.
+- Type 'quit' to quit (will automatically save your progress).
+- To fill in a box, type the row number, column number, and value separated by commas.
+- To erase a previously filled in box, type 'erase' in place of a value.
+"""
+def saveGrid(grid,dbName,gridName):
+    db = shelve.open(dbName)
+    db[gridName] = grid
+    db.close()           
+    print(f"\n{gcolors.OKCYAN}Grid has been saved!{gcolors.ENDC}")
 def main():
-    easy =  [   [' ',7,9,8,' ',2,' ',6,3],
+    """easy =  [   [' ',7,9,8,' ',2,' ',6,3],
                 [6,' ',' ',9,' ',' ',' ',1,' '],
                 [8,' ',3,' ',7,' ',' ',' ',2],
                 [' ',9,' ',' ',' ',' ',3,7,1],
@@ -13,16 +30,56 @@ def main():
                 [' ',3,1,' ',2,' ',5,8,' '],
                 [2,8,6,5,' ',' ',1,3,' '],
                 [' ',' ',' ',' ',' ',' ',' ',' ',' '],
-                [9,' ',4,3,' ',' ',8,2,7]]
-    grid = Grid(easy)
-    grid.display()
+                [9,' ',4,3,' ',' ',8,2,7]]"""
+    """easy2 =  [[' ',' ',7,1,5,4,3,9,6],
+                [9,6,5,3,2,7,1,4,8],
+                [3,4,1,6,8,9,7,5,2],
+                [5,9,3,4,6,8,2,7,1],
+                [4,7,2,5,1,3,6,8,9],
+                [6,1,8,9,7,2,4,3,5],
+                [7,8,6,2,3,5,9,1,4],
+                [1,5,4,7,9,6,8,2,3],
+                [2,3,9,8,4,1,5,6,7]]"""
+    dbName = 'appDB'
+    gridName = 'currentGridV3'
+    db = shelve.open(dbName)
+    grid = db[gridName]
+    db.close()
     
+    print('\n%s\n%s' % ("Welcome to Sudoku by Sarah!",helpString))
+    grid.display()
+
     while True:
-        if grid.isfull() == True: break
-        print("Row, Col, value: ")
-        row = input()
-        col = input()
-        val = input()
-        grid.fillbox(row,col,val)
+        if grid.isfull() == True: 
+            if grid.isCorrect() == True:
+                print(f"\n{gcolors.OKCYAN}YAY YOU DID IT!{gcolors.ENDC}\n")
+            else:
+                print(f"\n{gcolors.FAIL}Oh no! Something's wrong!{gcolors.ENDC}\n", file=sys.stderr)
+            break
+        text = str(input("\nRow, Col, value: ")).lower()
+        if text == 'quit':
+            saveGrid(grid,dbName,gridName)
+            print(f"\n{gcolors.OKCYAN}See ya!{gcolors.ENDC}\n")
+            break
+        if text == 'save':
+            saveGrid(grid,dbName,gridName)
+            grid.display()
+            continue
+        if text == 'help':
+            print(helpString)
+            grid.display()
+            continue
+        if text == 'reset':
+            grid.reset()
+            print(f"\n{gcolors.OKCYAN}Grid has been reset!{gcolors.ENDC}")
+            grid.display()
+            continue
+
+        try:
+            row, col, val = [x.lstrip().rstrip() for x in text.split(sep=',')]
+            grid.fillbox(row,col,val)
+        except ValueError:
+            print(f"\n{gcolors.FAIL}Error: Not enough values!{gcolors.ENDC}\n", file=sys.stderr)
+            grid.display()
 
 main()
